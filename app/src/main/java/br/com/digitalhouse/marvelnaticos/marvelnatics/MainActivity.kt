be59110e09da02adf3d64436c6cbe7ad2424792d
Piton <andre.elias.vieira@gmail.com>
2020-11-13 17:00:00 -0300

    rollback

--- app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/MainActivity.kt
+++ app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/MainActivity.kt
@@ -1,33 +1,11 @@
 package br.com.digitalhouse.marvelnaticos.marvelnatics
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.setContent
-import androidx.ui.tooling.preview.Preview
-import br.com.digitalhouse.marvelnaticos.marvelnatics.ui.CustomToolbar
-import br.com.digitalhouse.marvelnaticos.marvelnatics.ui.MarvelnaticsTheme
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MarvelnaticsTheme {
-                // A surface container using the 'background' color from the theme
-                Scaffold(topBar = {CustomToolbar()}, bodyContent = {})
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MarvelnaticsTheme {
-        CustomToolbar()
+        setContentView(R.layout.activity_main)
     }
 }
