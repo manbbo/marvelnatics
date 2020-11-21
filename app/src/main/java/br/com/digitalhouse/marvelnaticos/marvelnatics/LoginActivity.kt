7e04d82aab875d8f773ca4803b7a3af5b2d79670
Luiz Felipe Silva Braga <btmluiz@outlook.com>
2020-11-21 15:15:07 -0300

    Layout da tela de login

---
+++ app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/LoginActivity.kt
@@ -0,0 +1,12 @@
+package br.com.digitalhouse.marvelnaticos.marvelnatics
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_login)
+    }
+}
