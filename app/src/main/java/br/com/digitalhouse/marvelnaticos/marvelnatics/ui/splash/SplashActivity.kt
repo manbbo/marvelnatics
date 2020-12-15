dd1e5e9fe35fcd99092c2500dabcf6dd8765e795
Piton <andre.elias.vieira@gmail.com>
2020-12-15 14:32:41 -0300

    Adicionado zoom in e fade out na Splash. Também modificado a cor do fundo da Splash para AZUL

--- app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/ui/splash/SplashActivity.kt
+++ app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/ui/splash/SplashActivity.kt
@@ -4,13 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import br.com.digitalhouse.marvelnaticos.marvelnatics.R
 import br.com.digitalhouse.marvelnaticos.marvelnatics.ui.login.LoginActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val logo : ImageView = iv_splash_logo
+
+        // ZOOM IN
+        logo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.zoom_in_fade_out))
+
 
         Handler().postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
