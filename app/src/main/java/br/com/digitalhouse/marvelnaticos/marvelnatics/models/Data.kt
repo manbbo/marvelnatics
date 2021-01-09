664fbe883d3d828326c6a4f332588c6667bf44ae
matheusbsilva137 <matheus_barbosa137@hotmail.com>
2021-01-09 14:05:45 -0300

    Implementação da paginação e loading

--- app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/models/Data.kt
+++ app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/models/Data.kt
@@ -1,3 +1,3 @@
 package br.com.digitalhouse.marvelnaticos.marvelnatics.models
 
-data class Data(val offset: Int, var results: ArrayList<Comic>, var total: Int)
+data class Data(val offset: Int, var results: ArrayList<Comic?>, var total: Int, var count: Int)
