7392aeeba5f3bad00ead65db9227738451aefce8
Mateus Câmara <mc.email.1999@gmail.com>
2021-01-28 19:46:32 -0300

    Solução básica para o crash ao iniciar o app sem conexão com a internet Adição do NetworkViewModel para verificar o status da internet usando o LiveData

--- app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/ui/main/HomeViewModel.kt
+++ app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/ui/main/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package br.com.digitalhouse.marvelnaticos.marvelnatics.ui.main
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -20,18 +21,22 @@ class HomeViewModel(val repository: Repository) : ViewModel() {
 
     fun popListResult(query: String) {
         viewModelScope.launch {
-            var date = Date().toString()
-            var res = repository.getComics(
+            try {
+                var date = Date().toString()
+                var res = repository.getComics(
                     credentials.publicKey,
                     Utils.hashFormat(credentials.privateKey, credentials.publicKey, date),
                     date,
                     offset,
                     titleStartWith = query
-            )
+                )
 
-            offset++
-            listComics.value = res.data.results
-            totRes.value = res.data.total
+                offset++
+                listComics.value = res.data.results
+                totRes.value = res.data.total
+            } catch (ex: Exception) {
+                Log.e("MainViewModel", "Erro!", ex)
+            }
         }
     }
 }
