43820383a49636d19d31e5fb093bb107fce36129
Rodrigo Vaz <rodrigovaz_sg@hotmail.com>
2021-02-05 18:42:44 -0300

    Estatistica dos usuarios utilizando banco local

--- app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/ui/main/ProfileFragment.kt
+++ app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/ui/main/ProfileFragment.kt
@@ -2,39 +2,33 @@ package br.com.digitalhouse.marvelnaticos.marvelnatics.ui.main
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.viewModels
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.RecyclerView
 import br.com.digitalhouse.marvelnaticos.marvelnatics.FavoritesActivity
 import br.com.digitalhouse.marvelnaticos.marvelnatics.R
-import br.com.digitalhouse.marvelnaticos.marvelnatics.adapters.CharacterAdapter
-import br.com.digitalhouse.marvelnaticos.marvelnatics.models.Character
-import br.com.digitalhouse.marvelnaticos.marvelnatics.models.User
 import br.com.digitalhouse.marvelnaticos.marvelnatics.services.repo
 import br.com.digitalhouse.marvelnaticos.marvelnatics.ui.login.LoginActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import kotlinx.android.synthetic.main.fragment_profile.*
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
+
 class ProfileFragment : Fragment() {
 
-    lateinit var currentUser : FirebaseUser
+    lateinit var currentUser: FirebaseUser
+
     val viewModel: OfflineViewModel by viewModels<OfflineViewModel> {
         object : ViewModelProvider.Factory {
             override fun <T : ViewModel?> create(modelClass: Class<T>): T {
@@ -44,14 +38,23 @@ class ProfileFragment : Fragment() {
     }
 
     private lateinit var ctx: MainActivity
+    var queroLer = "0"
+    var tenho = "0"
+    var jaLi = "0"
+    var fav = "0"
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is MainActivity) ctx = context
 
+        viewModel.getStatistics()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val root = inflater.inflate(R.layout.fragment_profile, container, false)
 
         currentUser = FirebaseAuth.getInstance().currentUser!!
@@ -61,7 +64,11 @@ class ProfileFragment : Fragment() {
         Log.i("CURRENT USER", "onAttach: $currentUser")
 
         favoritos.setOnClickListener {
-            ctx.goToActivity(FavoritesActivity::class.java, R.anim.slide_in_right, R.anim.static_animation)
+            ctx.goToActivity(
+                FavoritesActivity::class.java,
+                R.anim.slide_in_right,
+                R.anim.static_animation
+            )
         }
 
         //Implementar regras p/ preencher os seguintes dados:
@@ -69,11 +76,16 @@ class ProfileFragment : Fragment() {
         val nameUser = if (currentUser.isAnonymous || currentUser == null) {
             "Fulano da Silva"
         } else currentUser.displayName
-        val imageUser =  if (!currentUser.isAnonymous && currentUser.photoUrl.toString().isNullOrEmpty()) currentUser.photoUrl else R.drawable.profilemock
-        val emailUser = if (currentUser.isAnonymous || currentUser == null) "e@mail" else currentUser.email
-        val readComics = "10% HQs lidas"
-        val readSeries = "20% Séries lidas"
-        val readCharacters = "7% Personagens vistos"
+        val imageUser = if (!currentUser.isAnonymous && currentUser.photoUrl.toString()
+                .isNullOrEmpty()
+        ) currentUser.photoUrl else R.drawable.profilemock
+        val emailUser =
+            if (currentUser.isAnonymous || currentUser == null) "e@mail" else currentUser.email
+
+
+        //Estatisticas do usuario
+        viewModel.popLists()
+
 
         val tv_nameUser: TextView = root.findViewById(R.id.tv_name_user)
         val tv_emailUser: TextView = root.findViewById(R.id.tv_email_user)
@@ -83,19 +95,40 @@ class ProfileFragment : Fragment() {
         val tv_readCharacters: TextView = root.findViewById(R.id.tv_read_characters)
         val btn_logout: TextView = root.findViewById(R.id.btn_logout)
 
+        var infoShared = "$nameUser's Statistics on Marvelnaltics: \n\n $queroLer \n $queroLer \n $queroLer \n\nYou can also keep track of your Marvel Comics with Marvelnatics!"
 
+        viewModel.statistics.observe(viewLifecycleOwner) {
+            queroLer = it["Q"].toString()
+            tenho = it["T"].toString()
+            jaLi = it["J"].toString()
+            fav = it["F"].toString()
+
+
+            tv_readComics.text = "Read: $jaLi comics"
+            tv_readSeries.text = "Possesses: $tenho comics"
+            tv_readCharacters.text = "Favorite: $fav comics"
+
+            infoShared=
+                "$nameUser's Statistics on Marvelnaltics: \n\n Read: $queroLer comics \n Possesses: $queroLer comics \n Favorite: $queroLer comics \n\nYou can also keep track of your Marvel Comics with Marvelnatics!"
+
+        }
+
+        viewModel.getStatistics()
+
+        tv_emailUser.text = emailUser
         tv_nameUser.text = nameUser
         //tv_photoUser.7
         Glide.with(root).load(imageUser)
-                .placeholder(R.drawable.profilemock)
-                .transition(DrawableTransitionOptions.withCrossFade())
-                .into(tv_photoUser)
-        tv_emailUser.text = emailUser
-        tv_readComics.text = readComics
-        tv_readSeries.text = readSeries
-        tv_readCharacters.text = readCharacters
+            .placeholder(R.drawable.profilemock)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(tv_photoUser)
 
-        val infoShared: String = "$nameUser's Statistics on Marvelnaltics: \n\n $readComics \n $readSeries \n $readCharacters \n\nYou can also keep track of your Marvel Comics with Marvelnatics!"
+
+
+        tv_readComics.text = "Read: $jaLi comics"
+        tv_readSeries.text = "Possesses: $tenho comics"
+        tv_readCharacters.text = "Favorite: $fav comics"
+
 
         val compartilhar: AppCompatImageButton = root.findViewById(R.id.bt_share_profile)
 
@@ -123,6 +156,7 @@ class ProfileFragment : Fragment() {
         return root
     }
 
+
     companion object {
 
         private const val ARG_SECTION_NUMBER = "section_number"
@@ -134,4 +168,6 @@ class ProfileFragment : Fragment() {
             }
         }
     }
+
+
 }
