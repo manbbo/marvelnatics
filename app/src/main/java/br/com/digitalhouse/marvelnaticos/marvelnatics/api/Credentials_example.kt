b3c958844727a78863a9a1174c2b842055ef0054
Rodrigo Vaz <rodrigovaz_sg@hotmail.com>
2021-02-08 15:19:41 -0300

    traducao das estatisticas para o portugues

--- app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/api/Credentials_example.kt
+++ app/src/main/java/br/com/digitalhouse/marvelnaticos/marvelnatics/api/Credentials_example.kt
@@ -13,3 +13,10 @@ class Credentials_example {
     val privateKey: String
         get() = "<CHAVE PRIVADA>"
 }
+
+class Credentials {
+    val publicKey: String
+        get() = "f5eddd8016824bcdfe8d0dffe9974247"
+    val privateKey: String
+        get() = "9dc84e3d0ae5f98a9d5a3d9e986bac8d1b4a62ea"
+}
